bd3f80068b52c825187caf3d1a8c358b28721f9f
Fabio Plunser <peppi@Fabio.localdomain>
2020-10-21 16:17:25 +0200

    cmake

---
+++ Programme/RLE-Encode/RLEV1/build/CMakeFiles/RLEV1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RLEV1.dir/main.cpp.o"
+  "RLEV1"
+  "RLEV1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RLEV1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Programme/RLE-Encode/RLEV1/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/peppi/FSST_Lezuo/Programme/RLE-Encode/RLEV1
+# Build directory: /home/peppi/FSST_Lezuo/Programme/RLE-Encode/RLEV1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
