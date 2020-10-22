d04989c8b900a180d5fba8f53a7eb8f06ddc188a
Fabio Plunser <peppi@Fabio.localdomain>
2020-10-22 17:58:22 +0200

    added POSIX copy

---
+++ Programme/Linux-Copy/build/CMakeFiles/Copy_with_POSIX.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Copy_with_POSIX.dir/src/copy_with_POSIX.c.o"
+  "Copy_with_POSIX"
+  "Copy_with_POSIX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Copy_with_POSIX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Programme/Linux-Copy/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/peppi/FSST_Lezuo/Programme/Linux-Copy
+# Build directory: /home/peppi/FSST_Lezuo/Programme/Linux-Copy/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
