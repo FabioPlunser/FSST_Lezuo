4b0d5c07c9eabb2848a52e6a772288cf60b4f7cd
Fabio Plunser <f.plunser@outlook.com>
2020-10-22 18:09:59 +0200

    update

--- Programme/RLE-Encode/RLEV1/src/RLE-Encode-V1.c
+++ Programme/RLE-Encode/RLEV1/src/RLE-Encode-V1.c
@@ -2,6 +2,7 @@
 // Date: 21.10.2020 //
 // GIT-Repo: https://github.com/FabioPlunser/FSST_Lezuo //
 // Specifig Git-location: https://github.com/FabioPlunser/FSST_Lezuo/blob/main/Programme/RLE-Encode/RLEV1/src/RLE-Encode-V1.c //
+
 // Compiled with Cmake, in WSL using Ubuntu 20.0.4, as you can see in my Repo //
 
 // RLE-Encode Version1//
