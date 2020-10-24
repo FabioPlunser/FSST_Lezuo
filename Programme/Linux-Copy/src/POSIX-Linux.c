429378633276bd9b236202e101db13f11865a874
Fabio Plunser <f.plunser@outlook.com>
2020-10-24 14:07:39 +0200

    commented POSIX

--- Programme/Linux-Copy/src/POSIX-Linux.c
+++ Programme/Linux-Copy/src/POSIX-Linux.c
@@ -1,44 +1,53 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/types.h>
-#define BUF_SIZE 20
+#include <unistd.h> 
+#include <fcntl.h> //needed for the correct addresses of the open, read, write commands
 
-int main(int argc, char **argv)
+#define BUF_SIZE 20 //open a file and read 20Bytes write 20Bytes repeat till complete source has been copied
+
+
+
+
+int copy(char* source, char*destination)
 {
-	int input_file, output_file, read_length, write_length;
-	char* buffer = malloc(BUF_SIZE);
+	int input_file, output_file, read_length, write_length; 
+	void* buffer = malloc(BUF_SIZE); //allocate Buffer
 	
 		
-	if(argc != 3){
-		printf("Usage: name of exe file1 file2\n");
-		return 1;
-	}
+	
 
-	input_file = open(argv[1], O_RDONLY); 
-	if (input_file == -1){
+	input_file = open(source, O_RDONLY | O_BINARY); //open source file, also if it's binary open it as binary file
+	if (input_file == -1){ //check if source file has been opened, if not print error: No such file or directory
 		perror("open");
 		return 2;
 	}	
 	
-	output_file = open(argv[2], (O_RDWR && O_TRUNC) | O_CREAT, 0644);
-	if (output_file == -1){
-		perror("open");
+	output_file = open(destination, (O_RDWR && O_TRUNC) | O_CREAT | O_BINARY, 0644); 	//open Source file as read/write and trunc to start at the beginning of the file
+	if (output_file == -1){																//o_creat => create file if doesn't exist yet | O_BINARY open it as a binary file 																					
+		perror("open");																	//0644 only needed for linux, so linux know the permission, 0644 is the normal user permission
 		return 3;
 	}
 	
-	
-	
-	while ((read_length = read(input_file, buffer, BUF_SIZE))>0){
-		write_length = write(output_file, buffer, read_length);
-		if(write_length != read_length){
+	while ((read_length = read(input_file, buffer, BUF_SIZE))>0){ 	//read source file and write it in the destination file, with 20Btyes at a time because buffer is only 20Bytes
+		write_length = write(output_file, buffer, read_length);		
+		if(write_length != read_length){							//if writing doesn't work print error
 			perror("write");
 			return 4; 
 		}
 	}
-	printf("Read %s and wrote it in %s file\n",buffer, argv[2]);
+	printf("Read %s and wrote it in %s file\n", buffer, destination);
+	
 	close(input_file);
 	close(output_file);
+	free(buffer);  //not really nedded because only 20Bytes big, but if programm runs on SOC you want to save as many memory as needed
+}
+
+
+int main(int argc, char **argv)
+{
+	if(argc != 3){
+		printf("Usage: name of exe sroucefile destinationfile\n");
+		return 1;
+	}
+	copy(argv[1], argv[2]);
 }
