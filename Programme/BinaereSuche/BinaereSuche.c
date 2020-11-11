0b7f48ef8e3d0e4ea13de71e0699d531c3ac4756
Fabio Plunser <peppi@Fabio.localdomain>
2020-11-11 11:12:56 +0100

    BinaereSuche

---
+++ Programme/BinaereSuche/BinaereSuche.c
@@ -0,0 +1,24 @@
+// Author: FabioPlunser //
+// Date: 24.10.2020 //
+// GIT-Repo: https://github.com/FabioPlunser/FSST_Lezuo
+// Specifig Git-location: https://github.com/FabioPlunser/FSST_Lezuo/tree/main/Programme/TCP_ECHO_Server //
+// Compiled with Cmake, in WSL using Ubuntu 20.0.4, as you can see in my Repo //
+
+// TCP-Echo-server //
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void* compare()
+{
+
+}
+
+int main()
+{
+
+}g
