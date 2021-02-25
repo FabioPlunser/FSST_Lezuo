5c134345bd7d063dd7dcad24dbe6e4f93d4b208c
Fabio Plunser <f.plunser@outlook.com>
2021-02-25 10:55:58 +0100

    finally string to hex function

--- Programme/openssl/openssl-Programm/main.c
+++ Programme/openssl/openssl-Programm/main.c
@@ -20,85 +20,155 @@
 #include <openssl/evp.h>
 #include <openssl/err.h>
 
-int do_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *iv, unsigned char* plaintext);
-int do_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext);
 
 
+void Error_handling(void)
+{
+    ERR_print_errors_fp(stderr);
+    abort();
+}
 
+int do_decrypt(char *ciphertext, int ciphertext_len, char *key, char *iv, char* plaintext)
+{
+    EVP_CIPHER_CTX *ctx;
+    int len;
+    int plaintext_len;
+    
+    if(!(ctx = EVP_CIPHER_CTX_new())) Error_handling();
+    
+    EVP_CIPHER_CTX_set_padding(ctx, 0);
+    
+    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv)) Error_handling();
 
-unsigned char* diyencryption (unsigned char* key, unsigned char* iv, unsigned char* plaintext, unsigned char ciphertext[128])
+    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) Error_handling();
+    plaintext_len = len;
+
+    if(1 != EVP_DecryptFinal_ex(ctx, plaintext+len, &len)) Error_handling();
+    plaintext_len += len;
+
+    ERR_print_errors_fp(stderr);
+    EVP_CIPHER_CTX_cleanup(ctx);
+    return plaintext_len;
+}
+
+int do_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext)
+{
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+
+    int ciphertext_len;
+    if(!(ctx = EVP_CIPHER_CTX_new())) Error_handling();
+
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv)) Error_handling();
+
+    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)) Error_handling();
+    ciphertext_len = len;
+
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) Error_handling();
+    ciphertext_len += len;
+    
+    EVP_CIPHER_CTX_free(ctx);
+
+    return ciphertext_len;
+}
+
+int diyencryption (unsigned char* key, unsigned char* iv, unsigned char* plaintext, int plaintext_len, unsigned char* ciphertext)
 {
     int ciphertext_len;
     ciphertext_len = do_encrypt(plaintext, strlen(plaintext), key, iv, ciphertext);
-    BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
-
-    // unsigned char* returnvalue;
-    // for(int i=0; i<ciphertext_len; i++)
-    // {
-    //     returnvalue[i] = ciphertext[i];
-    // }
-    // returnvalue[ciphertext_len] = '\0';
-    // return returnvalue;    
+    return ciphertext_len;
 }
 
-unsigned char* diydecryption(unsigned char* key, unsigned char* iv, unsigned char plaintext[128], unsigned char ciphertext[128])
+int diydecryption(unsigned char* key, unsigned char* iv, unsigned char* plaintext, unsigned char *ciphertext)
 {
-
-    unsigned char hex[128]={0};
-    char temp[2];
+    char temp[2];   
+    unsigned char *hex = malloc(sizeof(unsigned char)*128);
     int x = 0;
-    for(int i; i<strlen(ciphertext); i++)
+    for(int i=0; i<strlen(ciphertext); i++)
     {
         temp[0] = ciphertext[i];
         temp[1] = ciphertext[++i];
         temp[2] = '\0';
         hex[x] = (unsigned int)strtol(temp, NULL, 16);
         x++;
-        printf("Hex: %X", hex[x]);
     }
+   
+    int ciphertext_len = strlen(hex);
+    int decryptedtext_len;
+    decryptedtext_len = do_decrypt(hex, ciphertext_len, key, iv, plaintext);
+    plaintext[decryptedtext_len] = '\0'; 
 
-    // int decryptedtext_len;
-    // decryptedtext_len = do_decrypt(hex, sizeof(hex)/4, key, iv, plaintext);
-    // plaintext[decryptedtext_len] = '\0';
-    // printf("EVP: Decrypted test is: %s \n", plaintext);
-    
-    // AES_KEY dec_key;
-    // AES_set_decrypt_key(key, sizeof(key)*8, &dec_key);
-    // AES_cbc_encrypt(hex, plaintext, sizeof(hex)/4, &dec_key, iv, AES_DECRYPT);
-    // printf("AES_KEY: Decrypted test is: %s\n", plaintext);
 }
 int main()
-
 {   
-    char* sciphertext= "AAE365272C81078AB6116B361831D0F6A5D3C8587E946B530B7957543107F15E";
-    
-    unsigned char cipertext={0xAA, 0x34, }
-    unsigned char key[16];
-    unsigned char iv[16];
-
-    for(int i = 0; i<16; i++)
-    {
-        key[i] = 'B';
-        iv[i] = 'B';
-    }
-
-    unsigned char* Plaintext = "Schoene Crypto Welt";    
+    unsigned char* key = malloc(16); 
+    unsigned char* iv = malloc(16);
     unsigned char ciphertext[128];
-    unsigned char plaintextout[128];
-    diyencryption(key, iv, Plaintext, ciphertext);
-    printf("enryption ergebniss: ");
-    for(int i=0; i<strlen((char*)ciphertext)-4; i++)
+    unsigned char plaintext[128];
+    int choose;
+    printf("AES-128, encrypt und decrypt\n\n");
+    printf("1 für encrypt\n2 für decrypt\n3 encrypt Text aus Datei\n4 decrypt Hexzahl aus datei\n5 schließen\n\nEingabe:");
+    scanf("%i", &choose);
+    
+    if(choose == 1)
     {
-        printf("%X", ciphertext[i]);
-    }
-    printf("\n");
-    
-    // printf("Ciphertext TEST: %s", ciphertext);
-    // printf("\n\n");
-    // BIO_dump_fp (stdout, (const char *)ciphertext, strlen(ciphertext));
+        printf("Key: ");
+        scanf("%s", key);
 
-    diydecryption(key, iv, plaintextout, ciphertext);
-    // printf("Decrypted text is: %s\n", plaintextout);
-    
+        printf("IV: ");
+        scanf("%s", iv);
+
+        printf("Text der zu encrypten ist: ");
+        scanf("%s", plaintext);
+        printf("\n\n");
+
+        int plaintext_len = strlen(plaintext);
+        int ciphertext_len = diyencryption(key, iv, plaintext, plaintext_len, ciphertext);
+        printf("Enryption ergebniss: \n");
+        printf("\033[0;31m");
+        for(int i=0; i<ciphertext_len; i++)
+        {
+            printf("%02X", ciphertext[i]);
+        }
+        printf("\033[0m");
+        printf("\n");
+        main();
+    }
+    else if(choose == 2)
+    {
+        printf("Key: ");
+        scanf("%s", key);
+
+        printf("IV: ");
+        scanf("%s", iv);
+
+        printf("Ciphertext: ");
+        scanf("%s", ciphertext);
+        
+        diydecryption(key, iv, plaintext, ciphertext);
+        
+        printf("EVP: Decrypted test is: ");
+        printf("\033[0;31m");
+        printf("%s", plaintext);
+        printf("\033[0m");
+        printf("\n\n");
+
+        main();
+    }
+    else if(choose == 5)
+    {
+        printf("\033[0;31m");
+        printf("Wird geschlossen\n\n");
+        printf("\033[0m");
+        return 0;
+    }
+    else
+    {
+        printf("\033[0;31m");
+        printf("Es wurde keine mögliche Aktion ausgewählt\n\n");
+        printf("\033[0m");
+        return 0;
+    }
 }
     
